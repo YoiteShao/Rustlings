455e473dee7c4a433fe8d31df739564b4ccd11c5
YoiteShao <magic.sir@qq.com>
2025-01-19 22:37:56 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: u8) -> u8 {
+    match count {
+        c if c <= 40 => c * 2,
+        c => c,
+    }
+}
 
 // Don't modify this function!
 #[test]
